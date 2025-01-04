01027749519dbbb668170d2cc7213e45a8bda3af
longnt <88335051+LongNguyenT02@users.noreply.github.com>
2025-01-05 02:18:25 +0700

    update

--- src/app/api/cms/manageBannerApi.js
+++ src/app/api/cms/manageBannerApi.js
@@ -2,7 +2,19 @@ import ApiClient from "../ApiClient";
 const ManageBannerApi ={
     getPosition: async ()=>{
         return ApiClient("/cms/manageBanner/getPosition","GET");
-    }
+    },
+    createBanner: async (data)=>{
+        return ApiClient("/cms/manageBanner","POST",data);
+    },
+    editBanner: async (data)=>{
+        return ApiClient("/cms/manageBanner","PUT",data);
+    },
+    getAllBanner: async (page)=>{
+        return ApiClient("/cms/manageBanner?page="+page,"GET");
+    },
+    deleteBanner: async (data)=>{
+        return ApiClient("/cms/manageBanner/delete","POST",data);
+    },
 }
 
 export default ManageBannerApi;
---
+++ src/components/fe/home/section/Introduction.jsx
@@ -0,0 +1,20 @@
+import IntroductionImage from "@/assets/images/introduction.jpg"
+import Image from "next/image";
+import classes from "./Introduction.module.css"
+const Introduction = ()=>{
+    return <div className='flex max-w-[1170px] w-full gap-[20px] flex-wrap sm:flex-nowrap'>
+        <Image src={IntroductionImage} alt={"introduction image"} className='w-full sm:w-1/2 sm:h-[330px] object-cover' ></Image>
+        <div className='flex flex-col w-full sm:w-1/2' >
+            <p className={classes.title}>MEAT PLUS</p>
+            <p className={classes.content}>Ra đời từ 2003, Meat Plus – thương hiệu thịt nướng nổi tiếng đến từ Hàn Quốc nay đã có mặt tại hầu khắp
+                các quận nội thành của thủ đô. Thương hiệu này được biết đến với ba thế mạnh nổi bật: thịt bò Mỹ thượng
+                hạng nhập khẩu từ Hàn Quốc được sơ chế nghiêm ngặt theo quy trình HACPP; nguồn rau cuốn hữu cơ tự cung, được
+                trồng trên chính các nông trường của Meat Plus và sử dụng than Bijang loại than cao cấp không chứa lưu huỳnh độc
+                hại cho quá trình nướng. Vì lẽ đó, Meat Plus đã, đang và ngày càng chinh phục nhiều hơn khẩu vị của các thực khách,
+                những người sành ăn có niềm đam mê với ẩm thực nướng xứ Kim Chi.</p>
+            <p className={classes.pass}>Tư vấn nhượng quyền: <span className={classes.phone}>0328.666.782</span></p>
+        </div>
+    </div>
+}
+
+export default Introduction;
