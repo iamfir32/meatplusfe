dee394119c00da227ee0eb2b31510b95f451ba9f
longnt <88335051+LongNguyenT02@users.noreply.github.com>
2025-01-05 02:54:30 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost'],
+        domains: ['https://meatplusbe-production.up.railway.app/'],
     },
 };
 
