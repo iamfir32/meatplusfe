5b3c2d352ba5d264e435e6c66c5a16f2a7d9b420
longnt <88335051+LongNguyenT02@users.noreply.github.com>
2025-01-05 02:47:27 +0700

    update

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -16,7 +16,7 @@ export const authOptions = {
                 };
 
                 try {
-                    const res = await fetch("http://localhost:8080/auth/login", {
+                    const res = await fetch(process.env.NEXT_PUBLIC_API_URL+"/auth/login", {
                         method: 'POST',
                         body: JSON.stringify(data),
                         headers: {
