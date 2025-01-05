028dc7402968af1fae66b02571ff472d0ce15285
longnt <88335051+LongNguyenT02@users.noreply.github.com>
2025-01-05 09:48:24 +0700

    update

--- src/components/fe/home/section/Section.jsx
+++ src/components/fe/home/section/Section.jsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 import classes from "./SectionWrapper.module.css"
 import {getImageLink} from "@/utils/common";
 
-const Section = ({title,items})=>{
-    return <div className={classes.sectionContainer}>
+const Section = ({title,items,data})=>{
+    return <div className={`${classes.sectionContainer} homeSection ${data?.appearTransition}`}>
         <p className={'promo-title'}>{title}</p>
         <div className={classes.section}>
-            {items?.map((item,i)=>(item?.link?<Link className={classes.sectionItem} key={i} href={item?.link}>
+            {items?.map((item,i)=>(item?.link?<Link className={`${classes.sectionItem} ${classes.multiple}`} key={i} href={item?.link}>
                 <Image key={i} src={getImageLink(item?.imageUrl)} alt={"menu"} width={1920} height={1080} objectFit={'cover'}></Image>
-            </Link>:<Image className={classes.sectionItem} key={i} src={getImageLink(item?.imageUrl)} alt={"menu"} width={1920} height={1080} objectFit={'cover'}></Image>))}
+            </Link>:<Image className={`${classes.sectionItem} ${classes.multiple}`} key={i} src={getImageLink(item?.imageUrl)} alt={"menu"} width={1920} height={1080} objectFit={'cover'}></Image>))}
         </div>
     </div>
 }
