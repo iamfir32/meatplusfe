39903ecaeabdd30044210c24adeab8f7b1a3d858
longnt <88335051+LongNguyenT02@users.noreply.github.com>
2025-01-05 09:48:36 +0700

    update

--- src/components/fe/common/slider/Slider.jsx
+++ src/components/fe/common/slider/Slider.jsx
@@ -2,13 +2,13 @@
 import React, {useEffect, useState} from 'react';
 import { Carousel } from 'antd';
 import "./slider.css";
-const Slider =({items})=>{
+const Slider =({items, options})=>{
     const [isHydrated, setIsHydrated] = useState(false);
     useEffect(() => {
         setIsHydrated(true);
     }, []);
     return <>
-        {isHydrated && <Carousel arrows infinite={true} draggable>
+        {isHydrated && <Carousel arrows infinite={true} draggable {...options}>
             {items.map((item, i) => (
                 <div key={i} class={`flex justify-center items-center`} style={{display: 'flex !important'}}>
                     {item}
